cb6d451a68af6ac541a892b2b7c274d69ee207a4
zakiamansyah <zakiamansyah2014@gmail.com>
2022-08-01 02:15:03 +0700

    receiver messages

--- receiver.py
+++ receiver.py
@@ -1,3 +1,19 @@
 import pika
-import time
-import random
+
+def on_message_received(ch, method, properties, body):
+    print(f"received new message: {body}")
+
+connection_parameters = pika.ConnectionParameters('localhost')
+
+connection = pika.BlockingConnection(connection_parameters)
+
+channel = connection.channel()
+
+channel.queue_declare(queue='myBowl')
+
+channel.basic_consume(queue='myBowl', auto_ack=True,
+    on_message_callback=on_message_received)
+
+print("Starting Consuming")
+
+channel.start_consuming()
